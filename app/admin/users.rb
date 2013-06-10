3de397d13dac79fe602dee948c54db7d29bf3057
Marc MacLeod <marbemac@gmail.com>
2013-06-09 22:28:06 -0700

    allow adding practice name via admin
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -4,7 +4,8 @@ ActiveAdmin.register User do
 
   index do
     column "CBM User ID", :cbm_user_identifier
-    column :email                     
+    column :practice_name
+    column :email
     column :last_sign_in_at
     column :sign_in_count             
     default_actions                   
@@ -15,6 +16,7 @@ ActiveAdmin.register User do
   form do |f|                         
     f.inputs "Admin Details" do
       f.input :cbm_user_identifier, label: "CBM User ID"
+      f.input :practice_name
       f.input :email                  
       f.input :password               
       f.input :password_confirmation  
